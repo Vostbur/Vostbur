b7c6f7aee2b52013368ff78ddadffc930e2cf2e6
vostbur <vostbur@gmail.com>
2021-08-20 18:47:09 +0300

    add cisco badge attemp 2

--- get_last_posts.py
+++ get_last_posts.py
@@ -6,13 +6,7 @@ url = "https://vostbur.github.io"
 main_page = BeautifulSoup(requests.get(url).content, "html.parser")
 
 content = '''
-<div 
-    data-iframe-width="150" 
-    data-iframe-height="270" 
-    data-share-badge-id="185c22b1-6ad5-4b35-ab65-fb499041fb23" data-share-badge-host="https://www.credly.com">
-</div>
-<script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js">
-</script>
+[![](emerging-technologies-workshop-model-driven-programmability.png)](https://www.credly.com/badges/185c22b1-6ad5-4b35-ab65-fb499041fb23/public_url)
 
 '''
 content += f"### Last posts from [blog]({url}):\n\n"
