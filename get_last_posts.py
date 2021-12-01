ea3b2dff93dacc2e96a6aee54efbc22d9c810a2f
vostbur <vostbur@gmail.com>
2021-12-01 20:35:37 +0300

    add badge for cybersec intro

--- get_last_posts.py
+++ get_last_posts.py
@@ -7,6 +7,7 @@ main_page = BeautifulSoup(requests.get(url).content, "html.parser")
 
 content = '''
 [![](emerging-technologies-workshop-model-driven-programmability.png)](https://www.credly.com/badges/185c22b1-6ad5-4b35-ab65-fb499041fb23/public_url)
+[![](intro-to-cybersec.png)](https://www.credly.com/badges/0b0c9355-b236-4302-bdc5-aa3b6f8c9b8d/public_url/)
 
 '''
 content += f"### Last posts from [blog]({url}):\n\n"
