95834af5f44ec395f22004ac2f2e59cd6ec6a048
bautod <96714570+bautod@users.noreply.github.com>
2021-12-28 22:14:00 +0300

    Update get_last_posts.py

--- get_last_posts.py
+++ get_last_posts.py
@@ -5,15 +5,19 @@ from bs4 import BeautifulSoup
 url = "https://vostbur.github.io"
 main_page = BeautifulSoup(requests.get(url).content, "html.parser")
 
+
+# Bages from credly.com in header
 content = '''
 [![](emerging-technologies-workshop-model-driven-programmability.png)](https://www.credly.com/badges/185c22b1-6ad5-4b35-ab65-fb499041fb23/public_url)
 [![](intro-to-cybersec.png)](https://www.credly.com/badges/0b0c9355-b236-4302-bdc5-aa3b6f8c9b8d/public_url/)
 
 '''
+
 content += f"### Last posts from [blog]({url}):\n\n"
-for article_div in main_page.find_all("div", {"class": "mb-4"}):
-    article_url = article_div.find("a", href=True)
-    content += f"  - [{article_url.text}]({url + article_url['href']})\n"
+for article_main in main_page.find_all("article", {"class": "post-entry"}):
+    article_header = article_main.find("h2").text 
+    article_url = article_main.find("a", href=True)
+    content += f"  - [{article_header}]({article_url["href"]})\n"
 
 with open("README.md", "w", encoding="utf-8") as f:
     f.write(content)
